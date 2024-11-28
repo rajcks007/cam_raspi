df66c4dfddb29b164cdd03a6b30d48be853a0c67
rajcks007 <rajchoksi97@gmail.com>
2024-11-28 11:34:53 +0100

    ccolour_focus

--- cam_opencv/cam_colour_focus.py
+++ cam_opencv/cam_colour_focus.py
@@ -8,6 +8,7 @@ picam2 = Picamera2()
 
 picam2.start(show_preview=True)
 # picam2.start()
+picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 30.0})
 
 while(1):
     
@@ -20,14 +21,14 @@ while(1):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
  
     # define range of color in HSV
-    lower_colour = np.array([86,3,25])
-    upper_colour = np.array([255,160,122])
+    # lower_colour = np.array([86,3,25])
+    # upper_colour = np.array([255,160,122])
  
     # Threshold the HSV image to get only colors
     # mask = cv.inRange(hsv, lower_colour, upper_colour)
 
     # Threshold the GRAY image to get only colors
-    mask = cv.inRange(gray, 150, 255)
+    mask = cv.inRange(gray, 100, 255)
 
     # Bitwise-AND mask and original image
     res = cv.bitwise_and(image,image, mask= mask)
@@ -36,7 +37,7 @@ while(1):
     # cv.imshow('res',res)
     cv.imshow('mask',mask)
 
-cv.destroyAllWindows()
+# cv.destroyAllWindows()
  
-picam2.stop_preview()
-picam2.stop()
+# picam2.stop_preview()
+# picam2.stop()
