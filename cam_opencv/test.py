c5430c285cf13faebe6507b80e045d96a7910a95
rajcks007 <rajchoksi97@gmail.com>
2024-12-06 14:26:39 +0100

    change

--- cam_opencv/test.py
+++ cam_opencv/test.py
@@ -188,6 +188,8 @@ while(1):
 
 from digit_data import *
 
+print("for data_set 1")
+
 # Loop through each bit position (from 0 to 6)
 for bit_position in range(7):  # We have 7 bits, so positions 0 to 6
     found_one = False
@@ -202,6 +204,25 @@ for bit_position in range(7):  # We have 7 bits, so positions 0 to 6
         print(f"Bit position {bit_position} has at least one '1'.")
     else:
         print(f"Bit position {bit_position} has only '0' values.")
+
+print("for data_set 2")
+
+# Loop through each bit position (from 0 to 6)
+for bit_position in range(7):  # We have 7 bits, so positions 0 to 6
+    found_one = False
+    # Check each data point to see if the bit at the current position is '1'
+    for data_point in data_2:
+        if data_point[bit_position] == 1:
+            found_one = True
+            break  # No need to check further if we find at least one '1'
+        
+    # Print the result for the current bit position
+    if found_one:
+        print(f"Bit position {bit_position} has at least one '1'.")
+    else:
+        print(f"Bit position {bit_position} has only '0' values.")
+
+        
 
 picam2.stop_preview()
 picam2.stop()
